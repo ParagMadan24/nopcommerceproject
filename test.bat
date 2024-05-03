9035f129a9a0f111fe2458e98cdce38b25198ae8
Parag <paragmadan@outlook.com>
2024-05-04 00:36:29 +0530

    bat file changes

--- test.bat
+++ test.bat
@@ -1,2 +1,1 @@
-pytest -v -s testCases/test_customer_add.py --browser Chrome --html Reports/reports_Chrome.html;
-pytest -v -s testCases/test_customer_add.py --browser Chrome --html Reports/reports_Firefox.html;
+pytest -v -s testCases/test_customer_add.py --browser Chrome --html Reports/reports_Chrome.html
